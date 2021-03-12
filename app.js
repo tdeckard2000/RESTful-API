54800b04b6aaa04af9eaf0e25133844f5345eee7
TEDSPOWERHOUSE\Trent <tdecakrd2000@yahoo.com>
2021-03-11 19:14:50 -0700

    Finish building RESTful API

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 //Settings =============================================================================
 app.set('view engine', 'ejs');
-mongoose.connect('mongodb://localhost:27017/API', {useNewUrlParser: true, useUnifiedTopology: true});
+app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect('mongodb://localhost:27017/API', {useNewUrlParser: true, useUnifiedTopology: true})
+.catch((err)=>{console.log(err)});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
 db.once('open', function(){
@@ -27,16 +30,186 @@ const createArticle = function(title, content){
         content: content
     });
 
-    article.save((err, data)=>{
+    article.save((err, results)=>{
         if(err){
             console.warn(err);
         };
     });
 };
 
+const getAllArticles = function(){
+    return new Promise((resolve, reject)=>{
+        Article.find({},(err, results)=>{
+            if(err){
+                console.warn(err)
+                reject(err);
+            };
+            resolve(results);
+        });
+    })
+};
+
+const deleteOneArticle = function(articleTitle){
+    return new Promise((resolve, reject)=>{
+        Article.deleteOne({title:articleTitle}, (err, results)=>{
+            if(err){
+                console.warn(err);
+                reject(err);
+            }else{
+                resolve(results);
+            };
+        });
+    })
+};
+
+const getOneArticle = function(articleTitle){
+    return new Promise((resolve, reject)=>{
+        Article.findOne({title: articleTitle}, (err, results)=>{
+            if(err){
+                reject(err);
+            }else{
+                resolve(results);
+            };
+        });
+    });
+};
+
+const updateArticleContent = function(articleTitle, newTitle, newContent){
+    return new Promise((resolve, reject)=>{
+        Article.updateOne({title:articleTitle}, {title: newTitle, content: newContent}, (err, results)=>{
+            if(err){
+                console.warn(err);
+                reject(err);
+            }else{
+                resolve(results);
+            };
+        });
+    });
+};
+
+const patchArticleContent = function(title, newTitle, newContent){
+
+    if(newTitle === null || newTitle === ""){
+        //prevents field from becoming null or blank
+        //omitUndefined option catches undefined
+        title = undefined;
+    };
+
+    return new Promise((resolve, reject)=>{
+        Article.updateOne({title: title}, {content: newContent, title: newTitle}, {omitUndefined:true}, (err, results)=>{
+            if(err){
+                reject(err);
+            }else{
+                resolve(results);
+            };
+        });
+    });
+};
+
 //Get Requests =========================================================================
-app.get('/', (req, res)=>{
-    res.render("index.ejs");
+app.get('/articles', async (req, res)=>{
+    try {
+        const allArticles = await getAllArticles();
+        res.send(allArticles);
+    } catch(err) {
+        res.send("Error");
+    };
+    // res.render("allArticles.ejs", {allArticles:allArticles});
+});
+
+app.get('/articles/:title', async (req, res)=>{
+    const title = req.params.title;
+
+    try {
+        const result = await getOneArticle(title);
+
+        if(result){
+            res.send(result);
+        }else{
+            res.send("No articles found with that title.")
+        };
+    } catch(err){
+        res.send(err);
+    };
+});
+
+//Post Requests ========================================================================
+app.post('/articles', (req, res)=>{
+    const title = req.body.title;
+    const content = req.body.content;
+    createArticle(title, content);
+    res.send("Created " + title + " article.");
+});
+
+//Put Requests =========================================================================
+app.put('/articles/:title', async(req, res)=>{
+    const title = req.params.title;
+    const newTitle = req.body.title;
+    const newContent = req.body.content;
+
+    if(!newTitle){
+        res.send("Cannot replace title with an empty title.");
+        return;
+    };
+
+    try{
+        const result = await updateArticleContent(title, newTitle, newContent);
+        const articleFound = result.n;
+        const articleUpdated = result.nModified;
+
+        if(articleFound && articleUpdated){
+            res.send(title + " article was found and updated.")
+
+        }else if(articleFound && !articleUpdated){
+            res.send("Article was found, but no changes were made because\
+            the content and title were the same.");
+        }else{
+            res.send("The article " + title + " was not found, so could not be updated.");
+        };
+
+    }catch(err){
+        res.send(err);
+    };
+});
+
+//Patch Requests =======================================================================
+app.patch('/articles/:title', async(req, res)=>{
+    const title = req.params.title;
+    const newTitle = req.body.title;
+    const newContent = req.body.content;
+
+    if(!newTitle && !newContent){
+        res.send('No new title or content was provided.');
+        return;
+    };
+
+    try {
+        const result = await patchArticleContent(title, newTitle, newContent);
+        res.send("done")
+    } catch(err){
+        res.send(err);
+    }
+
+});
+
+
+//Delete Requests ======================================================================
+app.delete('/articles/:title', async(req, res)=>{
+    const title = req.params.title;
+    console.log(req.params)
+    try {
+        const result = await deleteOneArticle(title);
+        const numDeleted = result.deletedCount;
+
+        if(numDeleted > 0){
+            res.send("Deleted " + title + ".");
+        }else{
+            res.send("No articles deleted.")
+        };
+
+    } catch(err){
+        res.send("Error: " + err);
+    };
 });
 
 
